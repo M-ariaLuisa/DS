2f692f874a138b8f1b33a70bcaacdd2f6736f375
M-ariaLuisa <luisaoliveira643@gmail.com>
2026-08-24 14:34:15 -0300

    Add files via upload

---
+++ Ex08.java
@@ -0,0 +1,8 @@
+public class Ex08 {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+        for (int numero : numeros) {
+            System.out.println("Número: " + numero);
+        }
+}
+}
